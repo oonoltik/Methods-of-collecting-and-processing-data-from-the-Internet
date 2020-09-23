0bc5b2f856d36aee5dcdd50e9af7c342f68a7f2e
oonolitik <aanalitik@gmail.com>
2020-09-23 16:48:18 +0300

    ДЗ к УРоку 7 Scrapy Leroymerlin готовое

---
+++ runner.py
@@ -0,0 +1,18 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from leroymerlin import settings
+from leroymerlin.spiders.leroymerl import LeroymerlSpider
+
+
+
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(LeroymerlSpider)
+
+
+
+    process.start()
